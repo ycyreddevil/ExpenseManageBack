338cf253af6147bc53554e524b735dd7da0e536d
ycyreddevil <ycyreddevil@163.com>
2019-10-16 17:02:25 +0800

    第一次提交

---
+++ CustomModel/Response.cs
@@ -0,0 +1,23 @@
+﻿namespace ExpenseManageBack.CustomModel
+{
+    public class Response
+    {
+        public string message { get; set; }
+        
+        public int code { get; set; }
+
+        public Response()
+        {
+            this.message = "success";
+            this.code = 200;
+        }
+    }
+
+    public class Response<T> : Response
+    {
+        /// <summary>
+        /// 回传的结果
+        /// </summary>
+        public T Result { get; set; }
+    }
+}
