d743ca54b1414bee31ea64af30978514e0d6fd72
ycyreddevil <ycyreddevil@163.com>
2019-10-28 08:24:56 +0800

    完善审批流配置

---
+++ Model/TravelApply.cs
@@ -0,0 +1,10 @@
+using System.Reflection.Metadata;
+
+namespace ExpenseManageBack.Model
+{
+    public class TravelApply
+    {
+        public int Id { get; set; }
+        public string DocCode { get; set; }
+    }
+}
